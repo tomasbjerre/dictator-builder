a5c950df9ea760670f05a18bd2bee4836dd4e1a4
Tomas Bjerre <tomas.bjerre85@gmail.com>
2020-04-23 18:58:46 +0200

    work

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,30 @@
 #!/usr/bin/env node
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const path = require('path');
-const program = require('commander');
-const pkgUp = require('pkg-up');
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+const path = require("path");
+const program = require("commander");
+const pkgUp = require("pkg-up");
+const fs = require("fs");
 const packageJson = require(pkgUp.sync());
 
-const description = (packageJson.description ? packageJson.description + ". " : "")
+const DEFAULT_DICTATABLES_FOLDER = "dictatables";
+
+const description = packageJson.description
+  ? packageJson.description + ". "
+  : "";
 
 clear();
 console.log(
-  chalk.red(
-    figlet.textSync(packageJson.name, { horizontalLayout: 'full' })
-  )
+  chalk.green(figlet.textSync(packageJson.name, { horizontalLayout: "full" }))
 );
 
 program
   .version(packageJson.version)
   .description(description)
   .parse(process.argv);
+
+const dictatablesFolder = DEFAULT_DICTATABLES_FOLDER;
+if (!fs.existsSync(dictatablesFolder)) {
+  throw "Was unable to find folder: ${dictatablesFolder}";
+}
