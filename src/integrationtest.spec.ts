355c97f0f942adca3f1de030095b306b35077933
Tomas Bjerre <tomas.bjerre85@gmail.com>
2020-04-27 18:55:32 +0200

    work

--- src/integrationtest.spec.ts
+++ src/integrationtest.spec.ts
@@ -1,6 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+const copyfiles = require('copyfiles');
+var rimraf = require("rimraf");
+
 test('test examples', () => {
-    //Hitta exempel, för varje:
-    //  kopiera given till actual
-    //  mocka process.cwd() och kör mot actual
+  const examples = fs.readdirSync(path.join(__dirname, '..', 'examples'));
+  examples.forEach((example) => {
+    console.log(
+      `\n\n------------------------------------------------\n` +
+        `  Testing ${example}\n` +
+        `------------------------------------------------\n\n`
+    );
+    const given = path.join(example,'given')
+    const actual = path.join(example,'actual')
+    if (fs.existsSync(actual)) {
+        console.log(`Removing ${actual}`)
+        rimraf.sync(actual);
+    }
+    console.log(`Copy ${given} to ${actual}`)
+    copyfiles([given,actual])
+
+    console.log(`Stting process.cwd() to ${actual}`)
+    const spy = jest.spyOn(process, 'cwd');
+    spy.mockReturnValue(actual);
+
     //  jämför med expected
+  });
 });
