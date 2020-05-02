74143d1fd0d290ba9fa04eabd9b8ed457776a40a
Tomas Bjerre <tomas.bjerre85@gmail.com>
2020-05-02 10:24:32 +0200

    docs

--- src/dictatable/runningOnPlatform.ts
+++ src/dictatable/runningOnPlatform.ts
@@ -1,12 +1,13 @@
 import { Logger, LEVEL } from '../common/Logger';
+import { PLATFORM_TYPE } from '../types';
 
 export default function haveEnvironmentVariable(
   logger: Logger,
-  runningOnPlatform: string[]
+  runningOnPlatform: PLATFORM_TYPE[]
 ): boolean {
   logger.log(
     LEVEL.VERBOSE,
     `platform is ${process.platform} and wanted ${runningOnPlatform}`
   );
-  return runningOnPlatform.indexOf(process.platform) != -1;
+  return (runningOnPlatform as string[]).indexOf(process.platform) != -1;
 }
